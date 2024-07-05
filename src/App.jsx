200942d6f43b1ef82d7f28db9ffd4915037b6e95
Arteemka <artemiy.rak@mail.ru>
2024-07-05 16:14:38 +0300

    add container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>Welcome to React!</div>
-    );
-}
+  return (
+    <div className="container">
+      <section className="gallery">
+        <div className="frame">123</div>
+        <div className="frame">123</div>
+        <div className="frame">123</div>
+      </section>
+    </div>
+  );
+};
+
 export default App;
